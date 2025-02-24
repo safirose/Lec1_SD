43e699acbd12ca4d14f52d8492e07c57c2a34cec
Safi Rose Mollerup Møller Pedersen <safirose@MacBook-Air-tilhrende-Safi.local>
2025-02-24 11:36:26 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+       Circle circle = new Circle();
+       circle.x=5; circle.y=10; circle.radius=5;
+       System.out.println(circle.area());
+
+    }
+}
+
+ abstract class Shape {
+     int x;
+     int y;
+      abstract double area();
+
+    }
+    class Circle extends Shape {
+    double radius;
+    double area(){
+        return Math.PI * radius * radius;
+    }
+    class Rectangle extends Shape {
+        double length;
+        double width;
+        double area(){
+            return length * width;
+        }
+    }
+    }
---
+++ src/oevelse.java
@@ -0,0 +1,8 @@
+public class oevelse {
+    public static void main(String[] args) {
+        int [] count = new int [10];
+        for (int i = 0; i < count.length; i++) {
+
+        }
+    }
+}
